ad9355d918d269ec539415ff41ec1a15a1a85014
vdelgado <vinnygarciadelgado@gmail.com>
2022-02-14 18:31:04 -0300

    Genre detail

--- jetpackcompose-movies/app/src/main/java/com/vini/movies/presentation/screens/detail/DetailContent.kt
+++ jetpackcompose-movies/app/src/main/java/com/vini/movies/presentation/screens/detail/DetailContent.kt
@@ -4,16 +4,20 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,12 +26,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -38,9 +45,12 @@ import com.vini.movies.R
 import com.vini.movies.domain.model.Genre
 import com.vini.movies.domain.model.Movie
 import com.vini.movies.ui.theme.Black19
+import com.vini.movies.ui.theme.Blue17
+import com.vini.movies.ui.theme.EXTRA_SMALL_PADDING
 import com.vini.movies.ui.theme.LARGE_PADDING
 import com.vini.movies.ui.theme.MEDIUM_PADDING
 import com.vini.movies.ui.theme.PlayFont
+import com.vini.movies.ui.theme.SMALL_PADDING
 import com.vini.movies.ui.theme.WhiteE5
 
 @ExperimentalCoilApi
@@ -97,17 +107,27 @@ fun MovieGenreDataSection(movieGenres: List<Genre>) {
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
+                .padding(start = MEDIUM_PADDING, end = MEDIUM_PADDING),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             repeat(movieGenres.size) {
-                Text(
-                    modifier = Modifier.padding(start = MEDIUM_PADDING, end = MEDIUM_PADDING),
-                    text = movieGenres[it].name ?: "",
-                    fontFamily = PlayFont,
-                    lineHeight = 20.sp, color = textColor.copy(alpha = ContentAlpha.medium),
-                    fontSize = MaterialTheme.typography.subtitle2.fontSize,
-                    maxLines = 8,
-                    overflow = TextOverflow.Ellipsis
-                )
+                movieGenres[it].name?.let { name ->
+                    Text(
+                        modifier = Modifier
+                            .background(color = Blue17, shape = RoundedCornerShape(20.dp))
+                            .padding(
+                                start = EXTRA_SMALL_PADDING,
+                                end = EXTRA_SMALL_PADDING
+                            ),
+                        text = name,
+                        fontFamily = PlayFont,
+                        lineHeight = 20.sp, color = textColor.copy(alpha = ContentAlpha.medium),
+                        fontSize = MaterialTheme.typography.subtitle2.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        maxLines = 8,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
             }
         }
 
@@ -210,3 +230,28 @@ fun PreviewDetailScreen() {
         DetailScreen(navController = rememberNavController())
     }
 }
+
+@ExperimentalCoilApi
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun PreviewComponentTest() {
+    val textColor = if (isSystemInDarkTheme()) {
+        WhiteE5
+    } else {
+        Black19
+    }
+    Surface() {
+        Text(
+            modifier = Modifier
+                .background(color = Blue17, shape = RoundedCornerShape(20.dp))
+                .padding(20.dp, 20.dp),
+            text = "Adventure",
+            fontFamily = PlayFont,
+            lineHeight = 20.sp, color = textColor.copy(alpha = ContentAlpha.medium),
+            fontSize = MaterialTheme.typography.subtitle2.fontSize,
+            maxLines = 8,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
